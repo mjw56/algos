3c4e3077d8431a1251b8779722e7659241d1312e
Mike Wilcox <mwilcox56@gmail.com>
2016-03-02 21:42:02 -0500

    empty arr test

--- src/sorting/quicksort.js
+++ src/sorting/quicksort.js
@@ -77,3 +77,7 @@ export default function quicksort(arr, lo = 0, hi = arr.length - 1) {
 expect(
   quicksort([5, 2, 1, 8, 3, 9, 0, 4, 7, 6])
 ).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+expect(
+  quicksort([])
+).toEqual([]);
