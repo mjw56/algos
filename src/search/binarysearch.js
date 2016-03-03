ba83d596d56577a2606152b52f8e18da3bb5bffd
Mike Wilcox <mwilcox56@gmail.com>
2016-03-02 21:36:47 -0500

    jsdoc

--- src/search/binarysearch.js
+++ src/search/binarysearch.js
@@ -1,16 +1,23 @@
 import expect from 'expect';
 
 /**
- *
  * Binary Search Algorithm
  *
  * Worst case performance  O(log n)
  * Best case performance O(1)
  * Average case performance  O(log n)
  * Worst case space complexity O(1)
- *
  */
 
+ /**
+ * Finds the index of a value in an array
+ *
+ * @param {array} arr
+ * @param {number} value
+ * @param {?number} lo
+ * @param {?number} hi
+ * @returns {number}
+ */
 export default function binarySearch(arr, value, lo = 0, hi = arr.length - 1) {
   if (hi < lo) {
     return -1;
