59bae6f91887c1892a74611fe33ad5ae84ed2833
Mike Wilcox <mwilcox56@gmail.com>
2016-03-02 21:15:02 -0500

    handle empty array

--- src/search/binarysearch.js
+++ src/search/binarysearch.js
@@ -14,6 +14,10 @@ import expect from 'expect';
 export default function binarySearch(arr, value, lo = 0, hi = arr.length - 1) {
   const mid = Math.floor((hi + lo) / 2);
 
+  if (!arr || !arr.length) {
+    return -1;
+  }
+
   if (arr[mid] === value) {
     return mid;
   }
@@ -30,3 +34,4 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 expect(binarySearch(arr, 1)).toEqual(0);
 expect(binarySearch(arr, 5)).toEqual(4);
 expect(binarySearch(arr, 10)).toEqual(9);
+expect(binarySearch([], 10)).toEqual(-1);
