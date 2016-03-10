7fa5609d467d71946dc4be250bfa7090d51b3441
Mike Wilcox <mwilcox56@gmail.com>
2016-03-09 21:31:07 -0500

    remove expect assertions

--- src/sorting/mergesort.js
+++ src/sorting/mergesort.js
@@ -1,5 +1,3 @@
-import expect from 'expect';
-
 /**
  * Merge Sort Algorithm
  *
@@ -51,8 +49,3 @@ export default function mergeSort(arr) {
 
   return merge(mergeSort(left), mergeSort(right));
 }
-
-const arr = [2, 10, 9, 3, 1, 7, 6, 4, 8, 5];
-
-expect(mergeSort(arr))
-  .toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
