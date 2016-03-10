8218053017dc537ee5d2353c89c0e72b64ee33fd
Mike Wilcox <mwilcox56@gmail.com>
2016-03-09 21:30:53 -0500

    remove expect assertions

--- src/math/fibonacci.js
+++ src/math/fibonacci.js
@@ -1,10 +1,5 @@
-import expect from 'expect';
-
 export default function fibonacci(num) {
   if (num <= 1) return 1;
 
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
-
-expect(fibonacci(12))
-  .toEqual(233);
