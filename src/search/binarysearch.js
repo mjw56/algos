dddd6ad10240c324c44cbb1933fca6d3aff7d412
Mike Wilcox <mwilcox56@gmail.com>
2016-03-09 21:24:43 -0500

    remove expect test cases

--- src/search/binarysearch.js
+++ src/search/binarysearch.js
@@ -1,5 +1,3 @@
-import expect from 'expect';
-
 /**
  * Binary Search Algorithm
  *
@@ -39,12 +37,3 @@ export default function binarySearch(arr, value, lo = 0, hi = arr.length - 1) {
 
   return binarySearch(arr, value, mid + 1, hi);
 }
-
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-expect(binarySearch(arr, 1)).toEqual(0);
-expect(binarySearch(arr, 5)).toEqual(4);
-expect(binarySearch(arr, 10)).toEqual(9);
-expect(binarySearch([], 10)).toEqual(-1);
-expect(binarySearch(arr, 12)).toEqual(-1);
-expect(binarySearch(arr, 0)).toEqual(-1);
